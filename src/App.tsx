cb8dd2060ffa45e6f553dd8b321d467a1355d615
Rashid Ameer <r.ameer.offical@gmail.com>
2024-06-20 11:49:30 +0500

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Footer, Header } from "./components";
+
+function App() {
+  return (
+    <>
+      <Footer />
+      <Container />
+      <Header />
+    </>
+  );
+}
+
+export default App;
